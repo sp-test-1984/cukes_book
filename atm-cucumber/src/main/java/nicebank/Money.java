86e906a96be90b35e9d8eda2cc58a9a87ecad315
Juan Carlos Hernandez Iñiguez <juanhernandez@99.200-57-1.bestel.com.mx>
2018-10-18 14:35:57 -0500

    Money class added. It includes add and minus methods to be called  on credit and debit transactions respectivelly

---
+++ atm-cucumber/src/main/java/nicebank/Money.java
@@ -0,0 +1,69 @@
+package nicebank;
+
+public class Money {
+    // field to track dollars
+    private final int dollars;
+    private final int cents;
+
+    // no arg constructor
+    public Money(){
+        this.dollars = 0;
+        this.cents = 0;
+    }
+
+    // arg constructor
+    public Money(int dollars, int cents){
+        this.dollars = dollars;
+        this.cents = cents;
+    }
+
+    // accessors
+    public int getDollars() {
+        return dollars;
+    }
+
+    public int getCents() {
+        return cents;
+    }
+
+    // provide equals method, otherwise assertion will fail since
+    // comparison will verify if both arguments are the same object.
+    @Override
+    public boolean equals(Object other){
+        boolean equals = false;
+        if(other instanceof Money){
+            equals = this.getDollars() == ((Money) other).getDollars() &&
+                    this.getCents() == ((Money) other).getCents();
+
+        }
+        return equals;
+    }
+
+    // override to string
+    @Override
+    public String toString(){
+        return String.format("$%01d.%02d", this.getDollars(), this.getCents());
+    }
+
+    // credit
+    public Money add(Money amount){
+        int newCents = this.cents + amount.cents;
+        int newDollars = this.dollars + amount.dollars;
+        if(newCents >= 100){
+            newCents -= 100;
+            newDollars++;
+        }
+        return new Money(newDollars, newCents);
+    }
+
+    //debit
+    public Money minus(Money amount){
+        int newCents = this.cents - amount.cents;
+        int newDollars = this.dollars - amount.dollars;
+        if(newCents < 100){
+            newCents += 100;
+            newDollars--;
+        }
+        return new Money(newDollars, newCents);
+    }
+}
